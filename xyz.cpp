81b7939c616d939e6d404f2473010335e570cc17
Priyanshi Raj <priyanshiraj2909@gmail.com>
2021-06-08 16:25:11 +0530

    Add files via upload

---
+++ xyz.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    int a = 57;
+     cout << " the post increment operator is  " <<a++<<endl;
+     cout << " the pre increment operator is  " <<++a<<endl;
+     cout << " the no. on increasing by 1 is  " <<a + 1<<endl;
+     cout << " the post deccrement operator is  " <<a--<<endl;
+     cout << " the pre increment operator is  " <<--a<<endl;
+     cout << " the no. on deccreasing by 1 is  " <<a - 1<<endl;
+   
+    
+}
